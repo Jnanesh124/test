dff03a84372b5cf952684847090e2ffce30103ed
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-01-18 13:00:24 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -122,7 +122,7 @@ async def gen_link_batch(bot, message):
     post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="<b>📮Batch File Logs📮</b>")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
-    await sts.edit(f"<b>🎰 Here Is Your Link👇</b> \n<b>📚Contains </b>`{og_msg}` <b>Files</b>\n https://telegram.me/{temp.U_NAME}?start=BATCH-{file_id}</b>")
+    await sts.edit(f"<b>🎰 Here Is Your Link👇</b> \n<b>📚Contains </b>`{og_msg}` <b>Files</b>\n https://telegram.me/{temp.U_NAME}?start=BATCH-{file_id}</b> \n\n<a href="https://telegram.me/share/url?url=https://telegram.me/{temp.U_NAME}?start=BATCH-{file_id}"><b>Share Url 📲</b></a>")
 
 
     
