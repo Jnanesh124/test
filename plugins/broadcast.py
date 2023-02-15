c82bb94c6d9cc02ae19491713ac11aaa07d627a1
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-02-15 09:34:11 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -3,15 +3,15 @@ import datetime
 import time
 from database.users_chats_db import db
 from info import ADMINS
-from utils import broadcast_messages, groups_broadcast_messages
+from utils import broadcast_messages
 import asyncio
         
-@Client.on_message(filters.command("users_broadcast") & filters.user(ADMINS) & filters.reply)
-async def users_broadcast(bot, message):
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='Broadcasting your messages...'
+        text='📲 Broadcasting ♻️'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
@@ -35,44 +35,6 @@ async def users_broadcast(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done}</code> / <code>{total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>\nDeleted: <code>{deleted}</code>")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Users broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>\nDeleted: <code>{deleted}</code>")
-        
-
-@Client.on_message(filters.command("groups_broadcast") & filters.user(ADMINS) & filters.reply)
-async def groups_broadcast(bot, message):
-    chats = await db.get_all_chats()
-    b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
-    start_time = time.time()
-    total_chats = await db.total_chat_count()
-    done = 0
-    failed =0
-
-    success = 0
-    async for chat in chats:
-        pti, sh = await groups_broadcast_messages(int(chat['id']), b_msg)
-        if pti:
-            success += 1
-        elif pti == False:
-            if sh == "Error":
-                failed += 1
-        done += 1
-        await asyncio.sleep(2)
-        if not done % 20:
-            await sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done}</code> / <code>{total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
-
-
-
-
-
-
-
-
-
-
+            await sts.edit(f"📲<b> Broadcasting ♻️ </b>\n● Total Users » {total_users}\n● Completed » {done} / {total_users}\n● Success » {success}\n● Blocked » {blocked}\n● Deleted » {deleted}") 
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time)) 
+    await sts.edit(f"<b>📲 Broadcast Complete ✅</b>\n⌚ In {time_taken} Seconds\n\n● Total Users » {total_users}\n● Completed » {done} / {total_users}\n● Success » {success}\n● Blocked » {blocked}\n● Deleted » {deleted}")
